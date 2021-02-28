5d3626a5e786f459b905915e30b757bd84dc5999
Draylar <samuelhimes@gmail.com>
2021-02-28 17:44:32 -0600

    prevent simple dungeons from spawning in areas where they are no blocks above y35 (like empty spaces in the end)

--- src/main/java/draylar/dd/world/StandardDungeonStructure.java
+++ src/main/java/draylar/dd/world/StandardDungeonStructure.java
@@ -1,6 +1,12 @@
 package draylar.dd.world;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.gen.ChunkRandom;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
 
@@ -8,6 +14,11 @@ public class StandardDungeonStructure extends StructureFeature<DefaultFeatureCon
 
     public StandardDungeonStructure(Codec<DefaultFeatureConfig> codec) {
         super(codec);
+    }
+
+    @Override
+    public boolean shouldStartAt(ChunkGenerator chunkGenerator, BiomeSource biomeSource, long worldSeed, ChunkRandom random, int chunkX, int chunkZ, Biome biome, ChunkPos chunkPos, DefaultFeatureConfig config) {
+        return chunkGenerator.getHeightInGround(chunkX << 4, chunkZ << 4, Heightmap.Type.WORLD_SURFACE_WG) > 35;
     }
 
     @Override
